e29a5d2de63c1c1dee0547256805900e7db76239
Asaf Zamir <asaf.zamir@gmail.com>
2015-01-28 08:17:09 +0100

    updating load path

--- js/quiz-blocks/load.js
+++ js/quiz-blocks/load.js
@@ -20,17 +20,17 @@ Game.Load.prototype = {
 		this.stage.backgroundColor = "#000";
 		this.preloadtext = this.add.text(this.game.world.centerX,this.game.world.centerY,"Loading..."+this.load.progress+"%",{ font: "20px Arial", fill: "#ff0044", align: "center" });
 		this.preloadtext.anchor.setTo(0.5,0.5);
-
-		this.load.spritesheet('play','assets/play.png',100,80);
-		this.load.image('pause','assets/Pause.png');
-		this.load.image('reset','assets/refresh.png');
-		this.load.image('lose','assets/lose.png');
-		this.load.image('arrow','assets/arrow.png');
-		this.load.image('title','assets/Title.png');
-		this.load.image('logo','assets/logo2.png');
-		this.load.image('win','assets/win.png');
-		this.load.spritesheet('blocks','assets/blocks.png',30,30);
-		this.load.image('bck','assets/Bck.png');
+		var asset_dir = 'images';
+		this.load.spritesheet('play', asset_dir + '/play.png',100,80);
+		this.load.image('pause', asset_dir + '/Pause.png');
+		this.load.image('reset', asset_dir + '/refresh.png');
+		this.load.image('lose', asset_dir + '/lose.png');
+		this.load.image('arrow', asset_dir + '/arrow.png');
+		this.load.image('title', asset_dir + '/Title.png');
+		this.load.image('logo', asset_dir + '/logo2.png');
+		this.load.image('win', asset_dir + '/win.png');
+		this.load.spritesheet('blocks', asset_dir + '/blocks.png',30,30);
+		this.load.image('bck', asset_dir + '/Bck.png');
 	},
 
 	create : function(){
