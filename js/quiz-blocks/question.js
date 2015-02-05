8ab0f49909dfddfee16c170c1da003f7b6139a2e
Asaf Zamir <asaf.zamir@gmail.com>
2015-02-05 22:00:16 +0100

    Adding Question class, adding some basic functionality to present a random question + the possible answers

--- js/quiz-blocks/question.js
+++ js/quiz-blocks/question.js
@@ -1,12 +1,39 @@
-Question = function(category){
-	/*$.ajax({
-		url: "data/questions.json",
-		success: function (data) {
-		  var obj = JSON.parse(data);
+var questions;
+Question = function( game, category ){
+	this.game = game;
+	var that = this;
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200 ) {
+			questions = JSON.parse( xmlhttp.responseText );
+			that.setupQuestion();
 		}
-	});*/
+	}
+
+	xmlhttp.open( 'GET', 'data/questions.json', true );
+	xmlhttp.send();
+
 };
 
 Question.prototype = {
+	setupQuestion: function() {
+		var category = randomProperty( questions, true ); //Random Category
+		var question = randomProperty( questions[category] ); //Random Question
+		var question_title = question.question;
+		var answers = question.answers;
+		var question_title_style = { font: "24px Arial", fill: "#000", align: "left" };
+		var answers_style = { font: "16px Arial", fill: "#000", align: "left" };
+		this.game.add.text( 420, 20, question_title, question_title_style );
+		this.game.add.text( 420, 50, answers.join(), answers_style );
+	}
+};
 
+//Return random property of an object, if returnPropertyName is true - return property name
+function randomProperty( obj, returnPropertyName ) {
+	var keys = Object.keys(obj);
+	if ( typeof returnPropertyName !== 'undefined' && returnPropertyName ) {
+		return keys[ keys.length * Math.random() << 0];
+	}
+
+	return obj[keys[ keys.length * Math.random() << 0]];
 };
