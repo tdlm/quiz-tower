689cfb715911ab8044f1e100a63cbd12b2e726c8
Asaf Zamir <asaf.zamir@gmail.com>
2015-02-10 22:36:43 +0100

    Adding handler for answer clicks, splitting answers display

--- js/quiz-blocks.js
+++ js/quiz-blocks.js
@@ -893,13 +893,24 @@ Question = function( game, category ){
 Question.prototype = {
 	setupQuestion: function() {
 		var category = randomProperty( questions, true ); //Random Category
-		var question = randomProperty( questions[category] ); //Random Question
+		var question = randomProperty( questions[ category ] ); //Random Question
 		var question_title = question.question;
 		var answers = question.answers;
 		var question_title_style = { font: "24px Arial", fill: "#000", align: "left" };
 		var answers_style = { font: "16px Arial", fill: "#000", align: "left" };
 		this.game.add.text( 420, 20, question_title, question_title_style );
-		this.game.add.text( 420, 50, answers.join(), answers_style );
+		var answerLoc = 60;
+		for ( i in answers ) {
+			var answer = this.game.add.text( 420, answerLoc, answers[ i ], answers_style );
+			answer.inputEnabled = true;
+			//Event for answer click
+			answer.events.onInputDown.add( this.answerClick, this );
+			answerLoc += 40;
+		}
+	},
+	answerClick: function() {
+		//TODO: Handle right/wrong answers..
+		alert('BARF');
 	}
 };
 
